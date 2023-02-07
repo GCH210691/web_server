31043bd171c6d2a620c7e5158652a518b33ec744
GCH210691 <120078676+GCH210691@users.noreply.github.com>
2023-02-07 08:58:53 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+var http = require('http'); // 1 - Import Node.js core module
+
+const myServer =  (req, res) => {   // 2 - creating server
+
+    //handle incomming requests here..
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/student") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    
+    }
+    else
+        res.end('Invalid Request!');
+
+};
+
+var server = http.createServer(myServer)
+
+const PORT = 5000
+
+server.listen(PORT); //3 - listen for any incoming requests
+
+console.log('Server is up')
